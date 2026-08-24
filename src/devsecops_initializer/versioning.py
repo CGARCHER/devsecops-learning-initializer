1a154639d7f8308a00525510091993b2cd2f78f0
Cipri <cipri.profesor.informatica@gmail.com>
2026-08-24 20:57:02 +0200

    upgrade version

--- src/devsecops_initializer/versioning.py
+++ src/devsecops_initializer/versioning.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from typing import Literal
 
 
-DEVSECOPS_VERSION = "0.9.0"
+DEVSECOPS_VERSION = "0.9.1"
 VersionState = Literal["not_installed", "current", "update_available", "newer"]
 
 
